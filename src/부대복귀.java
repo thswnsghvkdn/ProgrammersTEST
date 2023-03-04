9b4f5b8b5ddc9b9b014a322d831b490fde7d8d78
DESKTOP-BIT8QND\wnsgh <ths3630802@naver.com>
2023-03-04 11:17:21 +0900

    목적지부터 시작하여 bfs 방식으로 모든 연결 노드를 탐색 하면서 한번 방문한 노드는 visit 처리하여 다시 방문하지 않도록한다.

--- src/부대복귀.java
+++ src/부대복귀.java
@@ -7,15 +7,23 @@ public class 부대복귀 {
 	int[] dp;
 	boolean[] visit;
 	LinkedList<Integer>[] roadArray;
+	Queue<Integer> roadQueue = new LinkedList<>();
+	
 	public int[] solution(int n, int[][] roads, int[] sources, int destination) {
 		dp = new int[n+1];
-		Arrays.fill(dp, -1);
 		visit = new boolean[n+1];
 		this.destination = destination;
 		initRoadArray(roads, n);
 		int[] answer = new int[sources.length];
+		roadQueue.add(destination);
+		visit[destination] = true;
+		findWay();
 		for(int i = 0 ; i < sources.length; i++) {
-			answer[i] = findWay(sources[i]);
+			if(sources[i] == destination) {
+				answer[i] = 0;
+				continue;
+			}
+			answer[i] = dp[sources[i]] == 0 ? -1 : dp[sources[i]];
 		}
 		return answer;
 	}
@@ -30,30 +38,18 @@ public class 부대복귀 {
 			roadArray[roadPair[1]].add(roadPair[0]);
 		}
 	}
-	private int findWay(int road) {
-		if(road == destination) {
-			return dp[road] = 0;
-		}
-		if(dp[road] != -1) {
-			return dp[road];
-		}
-		int shortestWay = -1;
-		for(int nextRoad : roadArray[road]) {
-			if(!visit[nextRoad]) {
-				visit[nextRoad] = true;
-				shortestWay = updateDistance( findWay(nextRoad) , shortestWay );
-				visit[nextRoad] = false;
+	private void findWay() {
+		while(!roadQueue.isEmpty()) {
+			int road = roadQueue.poll();
+			for(int nextRoad : roadArray[road]) {
+				if(!visit[nextRoad]) {
+					dp[nextRoad] = dp[road] + 1;
+					roadQueue.add(nextRoad);
+					visit[nextRoad] = true;
+				}
 			}
 		}
-		return dp[road] = shortestWay;
 	}
-	private int updateDistance(int newDist, int shortestWay) {
-		if(newDist == -1) {
-			return -1;
-		}
-		else {
-			if(shortestWay == -1) shortestWay = Integer.MAX_VALUE;
-			return Math.min(shortestWay,  newDist + 1);
-		}
-	}
+	
+
 }
