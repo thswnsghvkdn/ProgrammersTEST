0cc5a1b49e530ba4c15ed44dd6b9e417bb64bca6
DESKTOP-BIT8QND\wnsgh <ths3630802@naver.com>
2023-03-06 11:52:22 +0900

    첫째줄에 모든 경우의 수에 대해 나머지 모든 행은 이전 행을 0으로 만드는 것으로 반복하였으나 30% pass

--- src/고고학최고의발견.java
+++ src/고고학최고의발견.java
@@ -14,40 +14,43 @@ public class 고고학최고의발견 {
         		}
         	}
         }
-        dfs(clockHands, 0, firstZero);
+        dfs(clockHands, 0, firstZero,0);
         return shortestWay;
     }
 	
-	// depth 에 대해 0,0 ~ n,n 까지 한번씩 돌려보는 방법 주변에 0이 하나라도 있으면 시계를 돌리지 않는다.
-    private void dfs(int[][] clock, int depth, int zero) {
-    	for(int i = 0 ; i < mapRange; i++) {
-    		for(int j = 0; j < mapRange; j++) {
-    			int[][] copyClock = getCopy(clock);
-    			int newZero = turnClockHands(copyClock, i , j);
-    			if(newZero == 0) continue;
-    			if(zero + newZero == mapRange * mapRange) {
-    				shortestWay = Math.min(shortestWay, depth + 1);
-    				return;
+	// 0행에 대해 모든 경우의 수로 시계를 돌리고 나머지 행들을 이전 행을 0으로 변경시키기 위한 작업을 한다.
+    private void dfs(int[][] clock, int column, int zero, int turnedCount) {
+    	if(column == mapRange) return;
+    	for(int i = 0 ; i < 4; i++) {
+			int newZero = 0; 
+			int[][] copyClock = getCopy(clock);
+			if(i != 0) {  
+				newZero = turnClockHands(copyClock, 0 , column);
+			}
+			makeAllElementZero(copyClock, zero + newZero, turnedCount + i);
+			dfs(copyClock, column+1, zero + newZero, turnedCount + i);
+		}
+    }
+    private void makeAllElementZero(int[][] clock, int zero, int turnedCount) {
+    	int[][] copyClock = getCopy(clock);
+    	for(int i = 1; i < mapRange; i++ ) {
+    		for(int j = 0 ; j < mapRange; j++) {
+    			int turningCount = ( 4 - copyClock[i-1][j] ) % 4; // 0이 될만큼 회전해야 하는 수
+    			turnedCount += turningCount;
+    			for(int count = 0 ; count < turningCount; count++) { 
+    				zero += turnClockHands(copyClock, i, j);
     			}
-    			dfs(copyClock, depth+1, zero + newZero);
     		}
+    	}
+    	if(checkZero(copyClock)) {
+    		shortestWay = Math.min(shortestWay, turnedCount);
     	}
     }
     private int turnClockHands(int[][] clock, int row, int col) {
     	// 0으로 변화된 수
     	int zero = 0;
     	int[][] dir = { {0 , 0} , {1, 0} , {-1, 0} , {0, 1} , {0, -1} };
-    	// 주변에 0이 하나라도 있으면 pass
-    	for(int i = 0 ; i < 5 ; i++) {
-    		int newRow = row + dir[i][0];
-    		int newCol = col + dir[i][1];
-    		if(newRow < 0 || newRow >= mapRange || newCol < 0 || newCol >= mapRange ) {
-    			continue;
-    		} 
-    		if ( clock[newRow][newCol] == 0) {
-    			return zero;
-    		};
-    	}
+    	
     	for(int i = 0 ; i < 5 ; i++) {
     		int newRow = row + dir[i][0];
     		int newCol = col + dir[i][1];
@@ -68,5 +71,14 @@ public class 고고학최고의발견 {
     	    System.arraycopy(originalArray[i], 0, newArray[i], 0, originalArray[i].length);
     	}
     	return newArray;
+    }
+    
+    private boolean checkZero(int[][] clock) {
+    	for(int i = 0 ; i < mapRange; i++ ) {
+    		for(int j = 0 ; j < mapRange; j++) {
+    			if(clock[i][j] != 0) return false;
+    		}
+    	}
+    	return true;
     }
 }
