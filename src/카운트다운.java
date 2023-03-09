d1ded89f591d3425ec968ac6103310b7c3987a89
DESKTOP-BIT8QND\wnsgh <ths3630802@naver.com>
2023-03-09 21:08:41 +0900

    재귀호출에서 result 비교시 countAndSingleBull 객체 새로 생성하면서 진행

--- src/카운트다운.java
+++ src/카운트다운.java
@@ -1,11 +1,10 @@
 
 public class 카운트다운 {
     CountAndSingleBull[] DP = new CountAndSingleBull[100000];
-    int MAXVALUE = 10000000;
-    
+    final int maxValue = 987654321;
 	public int[] solution(int target) {
         initDart();
-        throwDart(target , 0, 0);
+        throwDart(target );
         return new int[] {DP[target].count, DP[target].singleBull};
     }
 	private void initDart() {
@@ -13,29 +12,33 @@ public class 카운트다운 {
 			DP[i] = new CountAndSingleBull(0,0);
 		}
 	}
-	private CountAndSingleBull throwDart(int target, int singleBull, int count) {
+	private CountAndSingleBull throwDart(int target) {
 		if(target == 0) {
-			return new CountAndSingleBull(count,singleBull);
+			return new CountAndSingleBull(0, 0);
 		}
 		if(target < 0 ) {
-			return new CountAndSingleBull(MAXVALUE, MAXVALUE);
+			return new CountAndSingleBull(maxValue, maxValue);
 		}
 		if(DP[target].count != 0) {
 			return DP[target];
 		}
-		CountAndSingleBull result = new CountAndSingleBull(MAXVALUE, MAXVALUE);
+		CountAndSingleBull result = new CountAndSingleBull(maxValue, maxValue);
 		int score;
 		for(int i = 21; i > 0 ; i--) {
 			// 불(50)과 싱글 포인트 점수에 대한 반복
 			score = ( i == 21 ? 50 : i);
-			result = getLeastOne(result, throwDart(target - score, singleBull + 1 , count + 1));
-			
+			CountAndSingleBull temp =  throwDart(target - score);
+			temp.count += 1;
+			temp.singleBull += 1;
+
+			result = getLeastOne(result, temp);
 		}
 		for(int i = 20 ; i > 0 ; i--) {
 			for(int j = 2 ; j <= 3; j++ ) {
 				score = i * j;
-				result = getLeastOne(result, throwDart(target - score, singleBull, count + 1));
-				
+				CountAndSingleBull temp =  throwDart(target - score);
+				temp.count += 1;
+				result = getLeastOne(result, temp);
 			}
 		}
 		return DP[target] = result;
@@ -46,12 +49,12 @@ public class 카운트다운 {
 			if(firstOne.singleBull > secondOne.singleBull) {
 				return firstOne;
 			}
-			return secondOne;
+			return new CountAndSingleBull(secondOne.count, secondOne.singleBull);
 		}
 		if(firstOne.count < secondOne.count) {
 			return firstOne;
 		}
-		return secondOne;	
+		return new CountAndSingleBull(secondOne.count, secondOne.singleBull);	
 	}
     // 최소 던진 횟수와 싱글 or bull 횟수
 	class CountAndSingleBull {
