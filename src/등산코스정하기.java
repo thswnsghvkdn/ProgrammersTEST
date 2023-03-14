be317b9ef752ba24c22f6ee59f511d1726b1dfe6
DESKTOP-BIT8QND\wnsgh <ths3630802@naver.com>
2023-03-14 15:25:23 +0900

    다익스트라 알고리즘 베이스로 접근하되 메모리 초과를 대비하여 hashset으로 visit 을 검사한다 다익스트라 알고리즘에서는 priority queue를 사용하여 최소 edge가 먼저 접근이 되도록 진행한다

--- src/등산코스정하기.java
+++ src/등산코스정하기.java
@@ -1,94 +1,90 @@
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Arrays;
 public class 등산코스정하기 {
-	ArrayList<Integer>[] courses;
-	boolean[] visit;
-	Map<Integer, Boolean> isGate;
-	Map<Integer, Boolean> isSummit;
-	int[][] climbIntense;
 	
-	// 목적지까지 최소 INTENSE
-	GoalAndIntense[] DP;
-	final int MAXVALUE = 10000000;
+	PriorityQueue<Edge> edges = new PriorityQueue<>();
+	Map<Integer, Edge> gates = new HashMap<>();
+	HashSet<Integer> isSummit = new HashSet<>();
+	HashSet<Integer> visit = new HashSet<>();
+	Map<Integer, ArrayList<Edge>> eachEdge = new HashMap<>();
+	int[] maxIntense;
+	Edge answer = new Edge(0,0, 10000001);
+	
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        int[] answer = {0,0};
-        initCourses(n + 1, paths, gates, summits);
-        GoalAndIntense result = new GoalAndIntense(MAXVALUE, MAXVALUE);
-        for(int gate : gates) {
-        	Arrays.fill(visit, false);
-        	updateResult(result, dfs(gate));
-        }
-        answer[0] = result.goal;
-        answer[1] = result.intense;
-        return answer;
-    }
-    private void initCourses(int n, int[][] paths,int[] gates, int[] summits) {
-    	courses = new ArrayList[n];
-    	visit = new boolean[n];
-    	DP = new GoalAndIntense[n];
-    	isGate = new HashMap<Integer, Boolean>();
-    	isSummit = new HashMap<Integer, Boolean>();
-    	climbIntense = new int[n][n];
-    	for(int i = 0 ; i < n ; i++) {
-    		courses[i] = new ArrayList<>();
-    		visit[i] = false;
-    		DP[i] = new GoalAndIntense ( MAXVALUE , MAXVALUE );
-    		isGate.put(i, false);
-    		isSummit.put(i, false);
-    		climbIntense[i] = new int[n];
-    	}
-    	for(int[] path : paths) {
-    		courses[path[0]].add(path[1]);
-    		courses[path[1]].add(path[0]);
-    		climbIntense[path[1]][path[0]] = path[2];
-    		climbIntense[path[0]][path[1]] = path[2];
-    	}
+    	init(n , paths, summits);
     	for(int gate : gates) {
-    		isGate.put(gate, true);
+    		edges.addAll(eachEdge.get(gate));
+    		visit.add(gate);
+    		maxIntense[gate] = 0;
+    	}
+    	while(!edges.isEmpty()) {
+    		Edge edge = edges.poll();
+    		if(visit.contains(edge.to)) {
+    			continue;
+    		}
+    		int intensity = Math.max(maxIntense[edge.from], edge.intense);
+    		if(maxIntense[edge.to] > intensity) {
+    			maxIntense[edge.to] = intensity;
+    		}
+    		if(isSummit.contains(edge.to)) {
+    			answer.update(new Edge(0, edge.to, intensity));
+    			continue;
+    		}
+    		visit.add(edge.to);
+    		edges.addAll(eachEdge.get(edge.to));
+    	}
+    	return new int[] { answer.to, answer.intense};
+    }
+    
+    
+    private void init(int n, int[][] paths, int[] summits) {
+
+    	maxIntense = new int[n+1];
+    	Arrays.fill(maxIntense, Integer.MAX_VALUE);
+    	for(int[] path: paths) {
+    		if(!eachEdge.containsKey(path[0])) {
+    			eachEdge.put(path[0], new ArrayList<>());		
+    		}
+    		eachEdge.get(path[0]).add(new Edge(path[0], path[1], path[2]));
+    		if(!eachEdge.containsKey(path[1])) {
+    			eachEdge.put(path[1], new ArrayList<>());		
+    		}
+    		eachEdge.get(path[1]).add(new Edge(path[1], path[0], path[2]));
+
     	}
     	for(int summit : summits) {
-    		isSummit.put(summit, true);
-    	}
-    }
-    private GoalAndIntense dfs(int path) {
-    	if(isSummit.get(path)) {
-    		return new GoalAndIntense(path, 0);
-    	}
-    	if(DP[path].goal != MAXVALUE) {
-    		return DP[path];
-    	}
-    	GoalAndIntense result = new GoalAndIntense(MAXVALUE, MAXVALUE);
-    	for(int nextPath : courses[path]) {
-    		if(isGate.get(nextPath) || visit[nextPath]) {
-    			continue;
-    		} 
-    		visit[nextPath] = true;
-    		GoalAndIntense temp = dfs(nextPath);
-            visit[nextPath] = false;
-    		temp.intense = Math.max(temp.intense, climbIntense[path][nextPath]);
-    		updateResult(result, temp);
-    	}
-    	return DP[path] = result;
-     }
-    // 강도가 낮은 목적지로 변경
-    // 강도가 같다면 번호가 낮은 목적지로 변경
-    private void updateResult(GoalAndIntense result, GoalAndIntense instance) {
-    	if(result.intense > instance.intense) {
-    		result.intense = instance.intense;
-    		result.goal = instance.goal;
-    	} else if(result.intense == instance.intense && result.goal > instance.goal) {
-    		result.goal = instance.goal;
+    		isSummit.add(summit);
     	}
     	
     }
-    // 목적지와 강도
-    class GoalAndIntense {
-    	int goal, intense;
-    	GoalAndIntense(int goal, int intense){
-    		this.goal = goal;
+    class Edge implements Comparable<Edge> {
+    	int from,to,intense;
+    	Edge(int from, int to, int intense) {
+    		this.from = from;
+    		this.to = to;
     		this.intense = intense;
+    	}
+    	public void update(Edge edge) {
+    		if(edge.intense < intense) {
+    			intense = edge.intense;
+    			to = edge.to;
+    			from = edge.from;
+    		} 
+    		if(edge.intense == intense && edge.to < to) {
+    			intense = edge.intense;
+    			to = edge.to;
+    			from = edge.from;	
+    		}
+    	}
+    	@Override 
+    	public int compareTo(Edge edge) {
+    		if(this.intense == edge.intense)
+    			return Integer.compare(this.to, edge.to);
+    		return Integer.compare(this.intense, edge.intense);
     	}
     }
 }
